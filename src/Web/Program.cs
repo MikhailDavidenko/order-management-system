20313f1ad86ec83aaded27ca077eddf027637b99
mikhail.davidenko <mikhail.davidenko@monopoly.su>
2025-01-24 00:57:49 +0300

    change services registration to AddUnitOfWork

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddContext();
-builder.Services.AddRepositories();
+builder.Services.AddUnitOfWork();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
